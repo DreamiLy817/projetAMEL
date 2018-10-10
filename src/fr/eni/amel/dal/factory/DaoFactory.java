73d7a2666fc01c01ca5b78967c135c0e5ff1b9d8
Milho-D <milho.deoliveira2017@campus-eni.fr>
2018-10-10 14:26:48 +0200

    UtilisateurDaoImpl et DaoFactory

--- src/fr/eni/amel/dal/factory/DaoFactory.java
+++ src/fr/eni/amel/dal/factory/DaoFactory.java
@@ -3,9 +3,11 @@ package fr.eni.amel.dal.factory;
 import fr.eni.amel.dal.PropositionDao;
 import fr.eni.amel.dal.QuestionDao;
 import fr.eni.amel.dal.TestDao;
+import fr.eni.amel.dal.UtilisateurDao;
 import fr.eni.amel.dal.impl.PropositionDaoImpl;
 import fr.eni.amel.dal.impl.QuestionDaoImpl;
 import fr.eni.amel.dal.impl.TestDaoImpl;
+import fr.eni.amel.dal.impl.UtilisateurDaoImpl;
 
 public class DaoFactory {
 	
@@ -21,4 +23,10 @@ public class DaoFactory {
  	public static QuestionDao questionDAO() {
 		return QuestionDaoImpl.getInstance();
 	}
+ 	
+ 	public static UtilisateurDao utilisateurDao() {
+		UtilisateurDao utilisateurDAO = new UtilisateurDaoImpl();
+ 		return utilisateurDAO;
+ 		
+ 	}
 }
